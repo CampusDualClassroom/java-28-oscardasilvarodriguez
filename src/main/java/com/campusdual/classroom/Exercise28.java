4791110910fa1f44dd473ef8c12a7dd27bb6e2b7
oscardasilvarodriguez <grelodj@hotmail.com>
2025-02-11 00:07:22 +0100

    ejercicio-28-java resuelto

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -2,6 +2,6 @@ package com.campusdual.classroom;
 
 public class Exercise28 {
     public static void main(String[] args) {
-
+        throw new RuntimeException("Esta es una excepción lanzada desde el método main");
     }
 }
